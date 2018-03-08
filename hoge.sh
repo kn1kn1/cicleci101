100fc0361741ed0e2e3f167ae10625943f1b2a74
kn1kn1 <kn1kn1@users.noreply.github.com>
2018-03-08 12:20:54 +0900

    Add expect

--- hoge.sh
+++ hoge.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+OUTPUT=$(expect -c '
+set timeout 5
+spawn env LANG=C sleep 60
+expect {
+  \"Do you want to continue (Y/n)? \" {
+    exit 0
+  }
+}
 exit 1
+')
+
+echo "Expect's return value : $?";
+echo "Expect Output : $OUTPUT"
+exit $?
